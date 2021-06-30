38d09fb840683c96f485b174f6240f928d0ad850
BrandonPerry-co <brandonperrymba@gmail.com>
2021-06-30 17:29:13 -0600

    Completed Project

--- src/Router.js
+++ src/Router.js
@@ -9,9 +9,22 @@ Remember to export the component at the end
 */
 
 import React from 'react'
-import { Switch, Route } from 'react-router'
+import { Switch, Route } from 'react-router';
+import Home from './components/Home';
+import About from './components/About';
+import Car from './components/Car';
 // Write component imports here //
 
+const Router = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/car/:id" component={Car} />
+        </Switch>
+    )
+}
 
+export default Router; 
 
 // Start Router function here //
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import cars from '../cars.json'
 // import material ui components here //
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
+import { useParams } from "react-router-dom"
+
+
 // Container, Paper, Chip //
 
-const Car = (props) => {
+const Car = (props) => {  
+const carInfo = props.match.params.id
+const {id, Name} = useParams();
+const params = useParams();
+
+let myCar = cars.find((car) => {
+    return car
+})
+
     return (
-        <h1>A specific car</h1>
-    )
+      <Container maxWidth="sm">
+        <Paper elevation={3} />
+        <h1>{`${myCar.Name}`} </h1>
+        <Chip label={`id: ${id}`}/>
+        <Chip label={`Name: ${myCar.Name}`}/>
+        <Chip label={`Miles Per Gallon: ${myCar.Miles_per_Gallon}`}/>
+        <Chip label={`Cylinders: ${myCar.Cylinders}`}/>
+        <Chip label={`Displacement: ${myCar.Displacement}`}/>
+        <Chip label={`Weight: ${myCar.Weight_in_lbs}`}/>
+        <Chip label={`Horsepower: ${myCar.Horsepower}`}/>
+        <Chip label={`Accleration: ${myCar.Acceleration}`}/>
+        <Chip label={`Year: ${myCar.Year}`}/>
+        <Chip label={`Origin: ${myCar.Origin}`}/>
+      </Container>
+    );
 }
 
-export default Car
+export default Car ;
